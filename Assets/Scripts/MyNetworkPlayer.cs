9842df93c05997f03d35c66fb79acc1c82964d7e
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-01-27 14:04:14 -0500

    Add SyncVar hooks to update players color and name

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -1,20 +1,34 @@
 using Mirror;
+using TMPro;
 using UnityEngine;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar] [SerializeField] private string displayName = "Missing Name";
-    [SyncVar] [SerializeField] private Color displayColour = Color.black;
+    [SyncVar(hook = nameof(HandleDisplayNameUpdated))] [SerializeField] private string displayName = "Missing Name";
+    [SyncVar(hook = nameof(HandleDisplayColorUpdated))] [SerializeField] private Color displayColour = Color.black;
+
+    [SerializeField] private TMP_Text displayNameText = null;
+    [SerializeField] private Renderer displayColourRenderer = null;
 
     [Server]
     public void SetDisplayName(string newDisplayName)
     {
         displayName = newDisplayName;
     }
-    
+
     [Server]
     public void SetDisplayColor(Color newDisplayColor)
     {
         displayColour = newDisplayColor;
+    }
+
+    private void HandleDisplayColorUpdated(Color oldColor, Color newColor)
+    {
+        displayColourRenderer.material.SetColor("_BaseColor", newColor);
+    }
+
+    private void HandleDisplayNameUpdated(string oldName, string newName)
+    {
+        displayNameText.text = newName;
     }
 }
